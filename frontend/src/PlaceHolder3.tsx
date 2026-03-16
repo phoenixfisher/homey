5d67897d0f4fbea8005ef5c206995fd40a0885f6
camjwebb <camjwebb8@gmail.com>
2026-03-16 08:28:53 -0600

    Placeholder Pages and TSX

---
+++ frontend/src/PlaceHolder3.tsx
@@ -0,0 +1,7 @@
+export default function PlaceHolder3() {
+  return (
+    <div>
+      <h1>PlaceHolder3</h1>
+    </div>
+  );
+}
---
+++ frontend/src/PlaceHolder4.tsx
@@ -0,0 +1,7 @@
+export default function PlaceHolder4() {
+  return (
+    <div>
+      <h1>PlaceHolder4</h1>
+    </div>
+  );
+}
